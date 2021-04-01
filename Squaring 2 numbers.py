80b9db240617f900c61ea75bac00cff90fb44d30
ammy194 <65552026+ammy194@users.noreply.github.com>
2021-04-01 18:59:11 +0530

    Add files via upload

---
+++ Squaring 2 numbers.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter a number : "))
+square = num1* num1
+print ("Your squared number is : ", square)
+#done
---
+++ To find Hypotenuse.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the number for your base : "))
+num2 = int(input("Enter second number for your perpendicular : "))
+import math
+num3 = (num1*num1) + (num2*num2)
+
+print("Your hypotenuse is = ",  (math.sqrt(num3)))
+#done
