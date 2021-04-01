f72e4c8818706d82bbdc640186b6085e67f97baa
ammy194 <65552026+ammy194@users.noreply.github.com>
2021-04-01 18:58:44 +0530

    Add files via upload

---
+++ Calculator basic.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+sum = num1 + num2
+print ("Your sum is", sum)
+difference = num1 - num2
+print ("Your difference is " , difference)
+product = num1 *num2
+print ("Your product is",product)
+quotient = num1 / num2
+print(" Your quotient is" ,quotient )
+#done
