67444aa8dd2008e653d45ea90a09874780d391b3
ammy194 <65552026+ammy194@users.noreply.github.com>
2021-04-07 15:12:21 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter  Math marks : "))
+num2 = int(input("Enter English marks : "))
+num3 = int(input("Enter Computer number : "))
+num4 = int(input("Enter Social number : "))
+num5 = int(input("Enter Hindi number : "))
+num6 = int(input("Enter Science number : "))
+
+sum = (num1 + num2 + num3 + num4 + num5 + num6) / 6
+print ("Your average is  = ", sum)
