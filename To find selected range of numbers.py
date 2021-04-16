b63ce3d5cee957d1a59fe17016a510f6e862df63
ammy194 <65552026+ammy194@users.noreply.github.com>
2021-04-16 13:36:23 +0530

    Add files via upload

---
+++ To find selected range of numbers.py
@@ -0,0 +1,14 @@
+# Python program to 
+# print natural number
+# using range
+  
+# printing a natural
+# number upto 20
+for i in range(1, 20):
+    print(i, end =" ")
+print()
+  
+# printing a natural
+# number from 5 t0 20
+for i in range(5, 20):
+    print(i, end =" ")
